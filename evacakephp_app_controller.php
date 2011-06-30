9cbb6a4a303357645528e88fcb1809bf2a3c71b5
Benny Leonard Enrico Panggabean <bendo01@yahoo.com.au>
2011-06-30 17:49:55 +0800

    remove toolbar component

--- evacakephp_app_controller.php
+++ evacakephp_app_controller.php
@@ -1,7 +1,9 @@
 <?php
 
 class EvacakephpAppController extends AppController {
-	var $components = array('RequestHandler','Session','Cookie','DebugKit.Toolbar');
+	//uncomment this if your using toolbar component
+	//var $components = array('RequestHandler','Session','Cookie','DebugKit.Toolbar');
+	var $components = array('RequestHandler','Session','Cookie');
 	var $helpers = array('Session','Js' => array('Jquery'),'Form', 'Html');
 }
 
